165cfc258f1cc0d28f8b417896c7718d22ae7cea
minhnguyen203 <133190731+minhnguyen203@users.noreply.github.com>
2023-09-12 10:59:33 +0700

    Add files via upload

---
+++ SQL.vd4.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE Display_Customers 
+AS 
+SELECT CustomerID, AccountNumber, CustomerType, rowguid, ModifiedDate 
+from Sales.Customer 
+//Kết quả: Command(s) completed successfully. 
+EXECUTE Display_Customers 
+//Kết quả: Hiển thị dữ liệu của bảng Customer
+EXECUTE xp_fileexist 'c:\myTest.txt'
+EXECUTE sys.sp_who
